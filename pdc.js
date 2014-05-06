f66f0583ccb03251c1ec5b55865bbc595beee821
pedro <pedro.muller@rulesware.com>
2014-05-06 10:54:43 -0600

    update latest version

--- pdc.js
+++ pdc.js
@@ -4,6 +4,7 @@ var xml2js = require('xml2js');
 var reader = require ("buffered-reader");
 var cache = require('memory-cache');
 var xslt4node = require('xslt4node');
+var enumerable = require('linq');
 
 function onRequest(request, response) {
 
@@ -19,26 +20,28 @@ function onRequest(request, response) {
 		}
 	};
 
-
-
   switch (request.url){
-   
     case ("/"):
-      getMapping(function(res){
-        finishRequest(  response , JSON.stringify(res) );
+      getJsonFromFile(function(res){
+        finishRequest(response,JSON.stringify(res, null, 4));
       });
     break;
+    case ("/map"):
+      getJsonFromFile(function(res){
+        enumerable.From(res).where(function(x){return x.task});
 
+
+        finishRequest(response,JSON.stringify(res, null, 4));
+      });
+    break;
     case ("/xml"):
 		xslt4node.transform(config, function (err) {
 		    if (err) {
 		        console.log(err);
 		    }
-		    finishRequest(  response , "done" );
+		    finishRequest(response , "done" );
 		});
-
     break;
-
     default:
       finishRequest(response, "404 Error");
       break;
@@ -46,18 +49,33 @@ function onRequest(request, response) {
 
 };
 
-var getMapping = function(callback){
+function nameProcessor(name) {
+  console.log(name);
+  console.log("/n");
+  return name;//.replace('bpmn2:', '');
+}
 
-      var parser = new xml2js.Parser();
-      fs.readFile('process.bpmn', function(err, data) {
-          parser.parseString(data, function (err, result) {
+var getJsonFromFile = function(callback){
+      if (cache.get("jsonResult")!=null){
+       callback(cache.get("jsonResult"))
+       console.log("element picked up from cache");
+      }
+      else{
+        var parser = new xml2js.Parser();
+        fs.readFile('process.bpmn', function(err, data) {
+          parser.parseString(data,{stripPrefix: "true"},
+            function (err, result) {
+            //if(err=!null){
+              console.log("2"); //finishRequest(response, "404 Error");
+            //}
+
             var jsonFile = JSON.stringify(result);
             var newResult = JSON.parse(jsonFile);
-            cache.put("mapping", newResult);
+            cache.put("jsonResult", newResult);
             callback( newResult );
           });
-      });
-    
+        });
+      }
 }
 
 function finishRequest(response, message){
@@ -68,3 +86,4 @@ function finishRequest(response, message){
 
 var server = http.createServer(onRequest);
 server.listen(8082);
+console.log("Server listening...");
