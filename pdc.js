c658f081a0beed217328f7715552620222263ba0
pedro <pedro.muller@rulesware.com>
2014-05-07 10:21:09 -0600

    added promises fucntionallity to the code

--- pdc.js
+++ pdc.js
@@ -4,7 +4,7 @@ var xml2js = require('xml2js');
 var reader = require ("buffered-reader");
 var cache = require('memory-cache');
 var xslt4node = require('xslt4node');
-var linq = require('node-linq').LINQ;
+var q = require('q'); 
 
 function onRequest(request, response) {
 
@@ -20,8 +20,6 @@ function onRequest(request, response) {
 		}
 	};
 
-
-
   switch (request.url){
    
     case ("/"):
@@ -30,13 +28,15 @@ function onRequest(request, response) {
       });
     break;
     case ("/map"):
-      getJsonFromFile(function(res){
-
-        var result = new linq(res).where(function(x){return x=="process"}).ToArray();
-
-
-        
+      getJsonFromFile().then(function(res){
+            console.log("getJsonFromFile:resolved")
+            var converterAsync = q(converter(res));
+            converterAsync.then(function(result){
+                console.log("converterAsync: "+result);
+                finishRequest(response, "mapping done");
+            })
       });
+
     break;
     case ("/xml"):
 		xslt4node.transform(config, function (err) {
@@ -45,15 +45,20 @@ function onRequest(request, response) {
 		    }
 		    finishRequest(  response , "done" );
 		});
-
     break;
-
     default:
       finishRequest(response, "404 Error");
       break;
   }
-
 };
+
+var converter =  function(input){
+  console.log("converter:resolved");
+ 
+  return "test";
+};
+ 
+
 
 function nameProcessor(name) {
   var prefixMatch = new RegExp(/(?!xmlns)^.*:/);
@@ -74,10 +79,11 @@ var getMapping = function(callback){
     
 }
 
-var getJsonFromFile = function(callback){
+var getJsonFromFile = function(){
+  var deferred = q.defer();
       if (cache.get("jsonResult")!=null){
-       callback(cache.get("jsonResult"))
        console.log("element picked up from cache");
+       deferred.resolve(cache.get("jsonResult"));
       }
       else{
         var parser = new xml2js.Parser({tagNameProcessors: [nameProcessor],attrNameProcessors: [nameProcessor]});
@@ -85,15 +91,14 @@ var getJsonFromFile = function(callback){
         fs.readFile('process.bpmn', function(err, data) {
           parser.parseString(data,
             function (err, result) {
-            
-
             var jsonFile = JSON.stringify(result);
             var newResult = JSON.parse(jsonFile);
             cache.put("jsonResult", newResult);
-            callback( newResult );
+            deferred.resolve(newResult);
           });
         });
       }
+  return deferred.promise;  
 }
 
 function finishRequest(response, message){
